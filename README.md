9dfa3c71fe8c5c3fb79efa25f0e162410f0d3dc1
Christopher Foster <cfosprof@gmail.com>
2023-02-20 15:24:31 -0800

    message

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # oddduck
 Code Fellows Week 3 201
 
+1. We need three products to vote for at a time
+2. After a week of data, we will make some graphs to visualize the results
+3. Three potential products, side by side--hey flot box
+4. None can be favored, so we should randomize the order they're displayed on the page.
+5. We'll need to manage the size and aspect rations of the images
+6. We need to store each vote as an anonymous vote
+  Then calculate the totals
+  then visually display the results
+7. No results should be shown on the page before 25 selections have been made
+8. The marketing team wants the total number of clicks, and the percentage of times an item was selected when it was shown.
+  keep track of how many times an item was displayed, and do the calculations
 
+
+Requirements:
+Create a Constructor function that creates an object associated with each product and has the following properties:
+  * name of the product
+  * file path of the image
+  * times the image has been shown.
+
+* Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
+
+* For each of the three images, increment its property of times it has been shown by one.
+
+* Attach an event listener to the section of the HTML page where the images are going to be displayed.
+
+* Once the users ‘clicks’ a product, generate three new products for the user to pick from.
+
+* As a user, I would like to track the selections made by viewers so that I can determine which products to begin production on.
+
+* In the constructor function define a property to hold the number of times a product has been clicked.
+
+* After every selection by the viewer, update the newly added property to reflect if it was clicked.
+
+* As a user, I would like to control the number of rounds a user is presented with so that I can control the voting session duration.
+* By default, the user should be presented with 25 rounds of voting before ending the session.
+* Keep the number of rounds in a variable to allow the number to be easily changed for debugging and testing purposes.
+* As a user, I would like to view a report of results after all rounds of voting have concluded so that I can evaluate which products were the most popular.
+* Create a property attached to the constructor function itself that keeps track of all the products that are currently being considered.
+
+* After voting rounds have been completed, remove the event listeners on the product.
+
+* Add a button with the text View Results, which when clicked displays the list of all the products followed by the votes received, and number of times seen for each. Example: banana had 3 votes, and was seen 5 times.
--- js/app.js
+++ js/app.js
@@ -1,2 +1,100 @@
 'use strict';
 
+let productContainer = document.querySelector('section');
+let resultButton = document.querySelector('button');
+let image1 = document.querySelector('.product-image-1');
+let image2 = document.querySelector('.product-image-2');
+let image3 = document.querySelector('.product-image-3');
+
+const state = {
+  products: [],
+  maxClicksAllowed: 25
+};
+
+
+// Constructor function for Product objects
+function Product(name, imagePath) {
+  this.name = name;
+  this.imagePath = imagePath;
+  this.views = 0;
+  this.votes = 0;
+}
+
+
+// Display the products
+function displayProducts() {
+  let product1, product2, product3;
+  //get random product
+  do {
+    product1 = state.products[Math.floor(Math.random() * state.products.length)];
+    product2 = state.products[Math.floor(Math.random() * state.products.length)];
+    product3 = state.products[Math.floor(Math.random() * state.products.length)];
+  // make sure the products are not the same
+  } while (product1 === product2 || product1 === product3 || product2 === product3);
+  //increment the times shown
+  product1.views++;
+  product2.views++;
+  product3.views++;
+  //display the products
+  image1.src = product1.imagePath;
+  image2.src = product2.imagePath;
+  image3.src = product3.imagePath;
+  image1.title = product1.name;
+  image2.title = product2.name;
+  image3.title = product3.name;
+}
+function handleProductClick(event){
+  if (event.target === productContainer) {
+    alert('Please click on an image');
+  } else {
+    for (let i = 0; i < state.products.length; i++) {
+      if (event.target.title === state.products[i].name) {
+        if (state.products[i].votes >= state.products[i].maxClicksAllowed) {
+          alert('You have already clicked this product the maximum number of times!');
+        } else {
+          /* Incrementing the votes property of the product object. */
+          state.products[i].votes++;
+        }
+        break;
+      }
+    }
+    let totalVotes = 0;
+    for (let i = 0; i < state.products.length; i++) {
+      totalVotes += state.products[i].votes;
+    }
+    if (totalVotes === state.maxClicksAllowed) {
+      productContainer.removeEventListener('click', handleProductClick);
+      resultButton.addEventListener('click', renderResults);
+      resultButton.className = 'clicks-allowed';
+      productContainer.className = 'no-voting';
+    } else {
+      displayProducts();
+    }
+  }
+}
+
+
+function renderResults() {
+  // If the voting rounds are over, display the results
+  let ul = document.querySelector('ul');
+  ul.innerHTML = ''; // clear any previous results
+  for (let i = 0; i < state.products.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${state.products[i].name} had ${state.products[i].votes} votes, and was shown ${state.products[i].views} times.`;
+    ul.appendChild(li);
+  }
+}
+
+/* A for loop to loop through the array of products */
+let candidates = ['bag.jpg','banana.jpg', 'bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg','pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg','unicorn.jpg', 'water-can.jpg','wine-glass.jpg'];
+
+for (let item of candidates) {
+  let name = item.split('.')[0];
+  let path = (`img/${item}`);
+  state.products.push(new Product(name, path));
+}
+console.log(state.products);
+
+displayProducts();
+
+productContainer.addEventListener('click', handleProductClick);
